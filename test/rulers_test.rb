213cbc39e38e43441aa133939c5cc76940d2c094
Andrew Morrison <morro141@gmail.com>
2018-01-09 21:37:07 -0500

    Fix README, basic testing, add functionality for arrays

--- test/rulers_test.rb
+++ test/rulers_test.rb
@@ -1,11 +1,27 @@
-require 'test_helper'
+require_relative 'test_helper'
+require_relative "../lib/rulers/array"
 
 class RulersTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Rulers::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def app
+    assert TestApp.new
+  end
+
+  def test_request
+  	get "/"
+
+  	assert last_response.ok?
+  	body = last_response.body
+  	assert body["Hello"]
+  end
+
+  def test_array
+  	arr = [1, 2, 3, 4, 5]
+
+  	assert arr.sum == 15
+
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,5 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rulers'
 
 require 'minitest/autorun'
+
+# Always using local Rulers first
